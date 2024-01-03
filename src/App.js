04518add4ca4dc10a91c8d1801b53c981814973d
nicol <nicolas.mahn@gmx.de>
2024-01-03 21:10:31 +0100

    Added Links to Impressum, Datenschutzerklaerung, and Spenden pages (hosted by AV website). Socials were also added.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import avLogo from './assets/AV_Logo_weiss.svg';
+import Layout from './Layout';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={avLogo} className="App-logo" alt="AV_Logo" />
-        <p>
-          Website Work in Progress
-        </p>
-      </header>
-    </div>
+    <Layout>
+      <div className="App">
+        <header className="App-header">
+          <img src={avLogo} className="App-logo" alt="AV_Logo" />
+          <p>
+            Website Work in Progress
+          </p>
+        </header>
+      </div>
+    </Layout>
   );
 }
 
---
+++ src/Layout.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Layout.css'; 
+import instagram from './assets/instagram.svg'
+import facebook from './assets/facebook.svg'
+
+const Layout = ({ children }) => {
+  return (
+    <div className="Layout">
+
+      <main>{children}</main>
+
+      <footer className="App-footer">
+        <div className="social-links">
+            <a href="https://www.instagram.com/activistsforthevictims_s/" target="_blank" rel="noopener noreferrer">
+                <img src={instagram} alt="Instagram" />
+            </a>
+            <a href="https://www.facebook.com/ActivistsForTheVictims" target="_blank" rel="noopener noreferrer">
+                <img src={facebook} alt="Facebook" />
+            </a>
+        </div>
+
+        <a href="https://www.activistsforthevictims.de/impressum/" target="_blank" rel="noopener noreferrer">Impressum</a>
+        <a href="https://www.activistsforthevictims.de/datenschutzerklaerung/" target="_blank" rel="noopener noreferrer">Datenschutzerklärung</a>
+        <a href="https://www.activistsforthevictims.de/spenden/" target="_blank" rel="noopener noreferrer">Spenden</a>
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
