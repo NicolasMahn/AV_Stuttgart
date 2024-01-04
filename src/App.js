44fa30f0afb16ea9f98be189c334dbfdc1ebce25
nicol <nicolas.mahn@gmx.de>
2024-01-04 19:51:58 +0100

    Finalised Link-Items

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import avLogo from './assets/AV_Logo_weiss.svg';
 import Layout from './layout/Layout';
 import LinkItem from './linkitem/LinkItem';
 import EarthlingEd from './assets/earthling_ed.jpg'
+import DE from './assets/Flag_of_Germany.svg'
+import EN from './assets/Flag_of_the_United_Kingdom.svg'
 import './App.css';
 
 function App() {
@@ -17,17 +19,45 @@ function App() {
         <div className="link-items-container">
           <LinkItem
             title="Eine Rede, die dir die Augen öffnet!"
+            description="Schau dir die aufrüttelnde Rede an, die Tausende von Studenten in Universitäten überall in Großbritannien hörten."
             url="https://www.youtube.com/watch?v=Z3u7hXpOm58&hl=de&cc_lang_pref=de&cc_load_policy=1"
             image={EarthlingEd}
-            dub="English"
-            sub="German"
+            dub={EN}
+            sub={DE}
+            dub_alt="EN"
+            sub_alt="DE"
           />
           <LinkItem
             title="Eine Rede, die dir die Augen öffnet!"
             url="https://www.youtube.com/watch?v=Z3u7hXpOm58&hl=de&cc_lang_pref=de&cc_load_policy=1"
             image={EarthlingEd}
-            dub="English"
-            sub="German"
+            dub={EN}
+            sub={DE}
+            dubAlt="EN"
+            subAlt="DE"
+          />
+          <LinkItem
+            title="Eine Rede, die dir die Augen öffnet!"
+            url="https://www.youtube.com/watch?v=Z3u7hXpOm58&hl=de&cc_lang_pref=de&cc_load_policy=1"
+            image={EarthlingEd}
+            sub={DE}
+            subAlt="DE"
+          />
+          <LinkItem
+            title="Eine Rede, die dir die Augen öffnet!"
+            url="https://www.youtube.com/watch?v=Z3u7hXpOm58&hl=de&cc_lang_pref=de&cc_load_policy=1"
+            image={EarthlingEd}
+            dub={EN}
+            dubAlt="EN"
+          />
+          <LinkItem
+            title="Eine Rede, die dir die Augen öffnet!"
+            url="https://www.youtube.com/watch?v=Z3u7hXpOm58&hl=de&cc_lang_pref=de&cc_load_policy=1"
+            image={EarthlingEd}
+          />
+          <LinkItem
+            title="Eine Rede, die dir die Augen öffnet!"
+            url="https://www.youtube.com/watch?v=Z3u7hXpOm58&hl=de&cc_lang_pref=de&cc_load_policy=1"
           />
         </div>
       </div>
--- src/linkitem/LinkItem.js
+++ src/linkitem/LinkItem.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import './LinkItem.css'; // Assuming you will create a separate CSS file for styling
+import './LinkItem.css'; 
 
-const LinkItem = ({ title, url, image, description, dub, sub }) => {
+const LinkItem = ({ title, url, image, description, dub, sub, dubAlt, subAlt }) => {
+  const hasImage = Boolean(image);
+
   return (
-    <div className="link-item">
-      {image && <img src={image} alt={title} className="link-image" />}
-      <div className="link-content">
-        <a href={url} target="_blank" rel="noopener noreferrer" className="link-title">{title}</a>
-        {description && <p className="link-description">{description}</p>}
-        {dub && <p className="link-dub">{dub}</p>}
-        {sub && <p className="link-dub">{sub}</p>}
-      </div>
-    </div>
+    <a href={url} target="_blank" rel="noopener noreferrer">
+        <div className="link-item">
+            <div className="link-image-container">
+                {hasImage ? (
+                    <img src={image} alt={title} className="link-image" />
+                ) : (
+                    <div className="link-image-spacer"></div>
+                )}
+            </div>
+            <div className="link-content">
+                <div className="link-title">{title}</div>
+                {description && <p className="link-description">{description}</p>}
+                <div className="language-container">
+                    {dub && sub && <div className="link-language">Dub&ensp;
+                        <img src={dub} alt={dubAlt} className="language-image"/> | Sub&ensp; 
+                        <img src={sub} alt={subAlt} className="language-image"/>
+                    </div>}
+                    {dub && !sub && <div className="link-language">Dub&ensp;<img src={dub} alt={dubAlt} className="language-image"/> </div>}
+                    {!dub && sub && <div className="link-language">Sub&ensp;<img src={sub} alt={subAlt} className="language-image"/></div>}
+                </div>
+            </div>
+        </div>
+    </a>
   );
 };
 
